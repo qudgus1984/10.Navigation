494c67ddb0918194c51d72be0df28ff82aa6f02d
ByeongHyeon <qudgus1984@naver.com>
2022-04-07 16:23:30 +0900

    11-5 완료

--- 10.Navigation/ViewController.swift
+++ 10.Navigation/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editButton" {
+            editViewController.textWayValue = "segue : use button"
+            // 버튼을 클릭한 경우
+        } else if segue.identifier == "editBarButton" {
+            editViewController.textWayValue = "segue : use Bar button"
+            // 바 버튼을 클릭한 경우
+        }
+    }
 
 
 }
