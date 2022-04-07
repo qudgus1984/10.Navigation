7a6913c1f87c33322e70e48d156c28b3484aa9fa
ByeongHyeon <qudgus1984@naver.com>
2022-04-07 17:09:35 +0900

    11-6 오류

--- 10.Navigation/EditViewController.swift
+++ 10.Navigation/EditViewController.swift
@@ -7,19 +7,32 @@
 
 import UIKit
 
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+}
+
 class EditViewController: UIViewController {
     
     var textWayValue: String = ""
+    var textMessage: String = ""
+    var delegate : EditDelegate?
+    // 수정화면에서 직접 텍스트 필드의 텍스트를 제어할 수 없기 때문에 변수 textMessage를 문자열로 만듬
 
     @IBOutlet var lblWay: UILabel!
+    @IBOutlet var txMessage: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         lblWay.text = textWayValue
+        txMessage.text = textMessage
+        // viewDidLoad 함수에서 변수 textMessage 값을 텍스트 필드의 텍스트로 대입하여 나타나게 함
     }
     
     @IBAction func btnDone(_ sender: UIButton) {
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txMessage.text!)
+        }
         _ = navigationController?.popViewController(animated: true)
     }
     
--- 10.Navigation/ViewController.swift
+++ 10.Navigation/ViewController.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, EditDelegate {
 
+    
+    @IBOutlet var txMessage: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let editViewController = segue.destination as! EditViewController
         if segue.identifier == "editButton" {
@@ -22,6 +26,12 @@ class ViewController: UIViewController {
             editViewController.textWayValue = "segue : use Bar button"
             // 바 버튼을 클릭한 경우
         }
+        editViewController.textMessage = txMessage.text!
+        editViewController.delegate = self
+    }
+    
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txMessage.text = message
     }
 
 
